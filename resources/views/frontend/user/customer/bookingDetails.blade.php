bd43e25d4e44922eb54086a2234ed884b98cf8d3
HareshRaidlayer <haresh@raidlayer.com>
2025-02-25 16:09:12 +0530

    booking
    booking

--- resources/views/frontend/user/customer/bookingDetails.blade.php
+++ resources/views/frontend/user/customer/bookingDetails.blade.php
@@ -5,32 +5,37 @@
     <div class="row gutters-16">
         <div class="col-xl-12 col-md-6 mb-4">
             <a class="back-button" href="{{route('dashboard')}}"><i class="las la-angle-left fs-14"></i> Back</a>
+            @if ($bookings)
             @foreach ($bookings as $booking)
-                <div class="card mt-3 p-3">
-                    <table class="w-full">
-                        <tr>
-                            <td><p class="fs-18 fw-700 text-dark mb-1">Hotel Name: <span class="fs-16 fw-500">{{ $booking->hotel->name}}</span></p></td>
-                            <td><p class="fs-18 fw-700 text-dark mb-1">Room Number: <span class="fs-16 fw-500">{{ $booking->room->room_number}}</span></p></td>
-                            <td><a class="btn btn-soft-secondary-base btn-icon btn-circle btn-sm hov-svg-white mt-2 mt-sm-0" href="{{ route('invoice.booking',['order_id'=>$booking->id])}}" title="Download Invoice">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12.001" viewBox="0 0 12 12.001">
-                                    <g id="Group_24807" data-name="Group 24807" transform="translate(-1341 -424.999)">
-                                      <path id="Union_17" data-name="Union 17" d="M13936.389,851.5l.707-.707,2.355,2.355V846h1v7.1l2.306-2.306.707.707-3.538,3.538Z" transform="translate(-12592.95 -421)" fill="#f3af3d"></path>
-                                      <rect id="Rectangle_18661" data-name="Rectangle 18661" width="12" height="1" transform="translate(1341 436)" fill="#f3af3d"></rect>
-                                    </g>
-                                </svg>
-                            </a></td>
-                        </tr>
-                        <tr>
-                            <td><p class="fs-18 fw-700 text-dark mb-1">CheckIn Date: <span class="fs-16 fw-500">{{ $booking->check_in_date}}</span></p></td>
-                            <td><p class="fs-18 fw-700 text-dark mb-1">CheckOut Date: <span class="fs-16 fw-500">{{ $booking->check_out_date}}</span></p></td>
-                        </tr>
-                        <tr>
-                            <td><p class="fs-18 fw-700 text-dark mb-1">Total Price: <span class="fs-16 fw-500">{{ single_price($booking->total_price)}}</span></p></td>
-                            <td><p class="fs-18 fw-700 text-dark mb-1">Booking code: <span class="fs-16 fw-500 text-primary">{{ $booking->code}}</span></p></td>
-                        </tr>
-                    </table>
-                </div>
-            @endforeach
+            <div class="card mt-3 p-3">
+                <table class="w-full">
+                    <tr>
+                        <td><p class="fs-18 fw-700 text-dark mb-1">Hotel Name: <span class="fs-16 fw-500">{{ $booking->hotel->name}}</span></p></td>
+                        <td><p class="fs-18 fw-700 text-dark mb-1">Room Number: <span class="fs-16 fw-500">{{ $booking->room->room_number}}</span></p></td>
+                        <td><a class="btn btn-soft-secondary-base btn-icon btn-circle btn-sm hov-svg-white mt-2 mt-sm-0" href="{{ route('invoice.booking',['order_id'=>$booking->id])}}" title="Download Invoice">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12.001" viewBox="0 0 12 12.001">
+                                <g id="Group_24807" data-name="Group 24807" transform="translate(-1341 -424.999)">
+                                  <path id="Union_17" data-name="Union 17" d="M13936.389,851.5l.707-.707,2.355,2.355V846h1v7.1l2.306-2.306.707.707-3.538,3.538Z" transform="translate(-12592.95 -421)" fill="#f3af3d"></path>
+                                  <rect id="Rectangle_18661" data-name="Rectangle 18661" width="12" height="1" transform="translate(1341 436)" fill="#f3af3d"></rect>
+                                </g>
+                            </svg>
+                        </a></td>
+                    </tr>
+                    <tr>
+                        <td><p class="fs-18 fw-700 text-dark mb-1">CheckIn Date: <span class="fs-16 fw-500">{{ $booking->check_in_date}}</span></p></td>
+                        <td><p class="fs-18 fw-700 text-dark mb-1">CheckOut Date: <span class="fs-16 fw-500">{{ $booking->check_out_date}}</span></p></td>
+                    </tr>
+                    <tr>
+                        <td><p class="fs-18 fw-700 text-dark mb-1">Total Price: <span class="fs-16 fw-500">{{ single_price($booking->total_price)}}</span></p></td>
+                        <td><p class="fs-18 fw-700 text-dark mb-1">Booking code: <span class="fs-16 fw-500 text-primary">{{ $booking->code}}</span></p></td>
+                    </tr>
+                </table>
+            </div>
+        @endforeach
+            @else
+            <p class="fs-18 fw-700 text-dark mb-1">Booking Details not found!</p>
+            @endif
+
         </div>
     </div>
 @endsection
